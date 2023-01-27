1a2ebecf233e1790ea2b026aa90c4024886f517e
sulonetskyy <sergiy.ulonetskyy@gmail.com>
2023-01-27 16:59:28 +0200

    Get data from environment

--- src/main/java/com/learn/graphql/resolver/query/BankAccountResolver.java
+++ src/main/java/com/learn/graphql/resolver/query/BankAccountResolver.java
@@ -4,10 +4,12 @@ import com.learn.graphql.model.BankAccount;
 import com.learn.graphql.model.Currency;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import graphql.schema.DataFetchingEnvironment;
+import graphql.schema.SelectedField;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -15,6 +17,12 @@ public class BankAccountResolver implements GraphQLQueryResolver {
 
     public BankAccount bankAccount(UUID id, DataFetchingEnvironment environment) {
         log.info("Retrieving bank account id: {}", id);
+
+        var requestedFields = environment.getSelectionSet().getFields().stream()
+                .map(SelectedField::getName)
+                .collect(Collectors.toUnmodifiableSet());
+        log.info("Requested fields: {}", requestedFields);
+
         return BankAccount.builder()
                 .id(id)
                 .currency(Currency.USD)
